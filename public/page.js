af8da5d076ae191f7094000c3cf2370feb8371ef
akanji688-ux <akanji688@gmail.com>
2026-06-09 02:48:35 +0700

    fix: set grid-template-columns dynamically = repeat(N, 1fr) for always 1 row

--- public/page.js
+++ public/page.js
@@ -159,6 +159,7 @@ function renderButtons(buttons, page) {
 
   const grid = document.getElementById('btnGrid');
   grid.innerHTML = '';
+  grid.style.gridTemplateColumns = `repeat(${buttons.length}, 1fr)`;
   buttons.forEach(b => {
     const a = document.createElement('a');
     a.href = b.link;
